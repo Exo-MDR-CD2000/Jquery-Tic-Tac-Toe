1fafaf0b5b0ad8f119848b9eb74ffd63d2b07a07
JAG24_Exo <joseguillen587@yahoo.com>
2024-09-11 18:51:21 -0700

    added template navbar and footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // jQuery goes here
+
+// Check if jQuery is loaded and working
+if (typeof jQuery !== 'undefined') {
+    console.log(`jQuery is loaded: version ${$.fn.jquery}`);
+} else {
+    console.log('jQuery is not loaded');
+}
+
+
+console.log($('body'));
